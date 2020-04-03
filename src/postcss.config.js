3be5cddb920b256d49dbd4739b9564c4ace5e5b0
joaosoaresaalm <joao.almeida@academico.ifpb.edu.br>
2020-04-03 18:51:33 -0300

    add files

---
+++ src/postcss.config.js
@@ -0,0 +1,16 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+      './src/*.html',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  })
+  
+  module.exports = {
+    plugins: [
+      require('tailwindcss'),
+      purgecss,
+      require('cssnano'),
+      require('autoprefixer'),
+    ]
+  }
